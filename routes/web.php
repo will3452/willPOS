463b5c3188cab3286882fd0535f5b6005d57fda1
unknown <williamgalas2@gmail.com>
2021-06-29 21:18:30 +0800

    add adminlte

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'willpos, comming soon!';
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
